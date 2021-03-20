ed08202d73a18f9b230c6b31caaede4ba8de93df
shareefmhd03 <80768690+shareefmhd03@users.noreply.github.com>
2021-03-20 09:57:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
     $("#contactForm").validate({
         rules:{
             name:{
@@ -18,7 +18,7 @@
             
         }
     })
-})*/
+})
 
 
 
